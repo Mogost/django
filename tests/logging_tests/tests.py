8429001bbc70c435e4e0038b0b009086b9deb1e6
Adam Johnson <me@adamj.eu>
2021-01-18 15:11:12 +0000

    Added exception to SuspiciousOperation logging.
    The other logging calls from `response_for_exception()` include the exception, it seems to only have been missed here. This allows better debugging and filtering of errors.

--- tests/logging_tests/tests.py
+++ tests/logging_tests/tests.py
@@ -6,7 +6,9 @@ from admin_scripts.tests import AdminScriptTestCase
 
 from django.conf import settings
 from django.core import mail
-from django.core.exceptions import PermissionDenied
+from django.core.exceptions import (
+    DisallowedHost, PermissionDenied, SuspiciousOperation,
+)
 from django.core.files.temp import NamedTemporaryFile
 from django.core.management import color
 from django.http.multipartparser import MultiPartParserError
@@ -487,6 +489,7 @@ class SecurityLoggerTest(LoggingAssertionMixin, SimpleTestCase):
             msg='dubious',
             status_code=400,
             logger='django.security.SuspiciousOperation',
+            exc_class=SuspiciousOperation,
         )
 
     def test_suspicious_operation_uses_sublogger(self):
@@ -496,6 +499,7 @@ class SecurityLoggerTest(LoggingAssertionMixin, SimpleTestCase):
             msg='dubious',
             status_code=400,
             logger='django.security.DisallowedHost',
+            exc_class=DisallowedHost,
         )
 
     @override_settings(
@@ -505,7 +509,7 @@ class SecurityLoggerTest(LoggingAssertionMixin, SimpleTestCase):
     def test_suspicious_email_admins(self):
         self.client.get('/suspicious/')
         self.assertEqual(len(mail.outbox), 1)
-        self.assertIn('Report at /suspicious/', mail.outbox[0].body)
+        self.assertIn('SuspiciousOperation at /suspicious/', mail.outbox[0].body)
 
 
 class SettingsCustomLoggingTest(AdminScriptTestCase):
